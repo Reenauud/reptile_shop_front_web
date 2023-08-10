b12c64db4d83d12c1b6196f7428c32a834281de3
reenauud <renaud.raymond43@hotmail.fr>
2023-08-10 14:32:32 +0200

    1

--- .github/workflows/deploy.reptiles_shop_web.sh
+++ .github/workflows/deploy.reptiles_shop_web.sh
@@ -23,7 +23,7 @@ mv ../dotenv/reptile_shop_front_web/.env.frontend .env
 docker compose -f docker-compose.yml build --no-cache
 
 # start containers
-docker compose -f docker-compose.yml up >~/logs/reptile_shop_front_web/log.compose.$(date +"%s") 2>&1 &
+docker compose -f docker-compose.yml up >~/logs/reptile_shop_front/log.compose.$(date +"%s") 2>&1 &
 disown
 
 # delete old folder
