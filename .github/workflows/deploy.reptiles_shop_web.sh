9a5229cabdc604023fc4a08907d98aa9f15bc825
reenauud <renaud.raymond43@hotmail.fr>
2023-08-10 14:17:26 +0200

    ,

--- .github/workflows/deploy.reptiles_shop_web.sh
+++ .github/workflows/deploy.reptiles_shop_web.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # prune docker
-docker stop $(docker ps --filter status=running --filter name=reptile_shop_front_web -q)
+docker stop $(docker ps --filter status=running --filter name=reptile_shop_web -q)
 docker rm -f $(docker ps --filter status=exited -q)
-docker rmi -f $(docker images reptile_shop_front_web* -q)
+docker rmi -f $(docker images reptile_shop_web* -q)
 docker image prune -f
 
 # fix for "Permission denied" error
-sudo chmod -R 777 reptile_shop_front_web/
+sudo chmod -R 777 reptile_shop_web/
 
 # prepare new deployment folder
-mv reptile_shop_front_web/ old_reptile_shop_front_web/
+mv reptile_shop_web/ old_reptile_shop_web/
 git clone git@github.com:Reenauud/reptile_shop_front_web.git
-cd reptile_shop_front_web/
+cd reptile_shop_web/
 git pull -f --rebase origin main
 
 # récupérer les .env uploadés précédemment avec scp et les déplacer ici
@@ -27,4 +27,4 @@ docker compose -f docker-compose.yml up >~/logs/reptile_shop_web/log.compose.$(d
 disown
 
 # delete old folder
-sudo rm -Rf ~/old_reptile_shop_front_web/
+sudo rm -Rf ~/old_reptile_shop_web/
